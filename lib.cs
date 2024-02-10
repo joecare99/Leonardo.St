a043b5b400230cebddcfd9efaeac9962f7f6fd60
2alf <113948114+2alf@users.noreply.github.com>
2024-02-11 00:13:11 +0100

    init

---
+++ lib.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Xml.Linq;
+using System.Drawing.Drawing2D;
+using System.Security;
+using System.Net;
+using System.Configuration;
+using Newtonsoft.Json.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Leonardo
+{
+	// [Redacted for security]
+	// Snippets coming soon
+}
