c50e83c4eaf41dedf0adb2f4b396e24768092b84
Joe Care <Joe_care_99@yahoo.com>
2026-09-23 22:54:58 +0200

    Leonardo.Ava

---
+++ Leonardo.Wpf/Models/SettingsProxy.cs
@@ -0,0 +1,18 @@
+﻿using Leonardo.Models.Interfaces;
+
+namespace Leonardo.Models
+{
+    internal class SettingsProxy : ILeonardoSettings
+    {
+        public string this[ELSetting key] { get => Get(key); set => throw new System.NotImplementedException(); }
+
+        public string Get(ELSetting key)
+        {
+            return key switch
+            {
+                ELSetting.ApiToken => "YourApiTokenHere",
+                _ => throw new System.NotImplementedException(),
+            };
+        }
+    }
+}
